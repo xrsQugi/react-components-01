a192512463832311ff9a028a10719daef93b245d
xrsQugi <02.04.5.art@gmail.com>
2023-06-11 19:53:22 +0300

    add styles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,38 @@
+import css from "./Profile.module.css";
+import PropTypes from "prop-types";
+
 export default function Profile({ username="Someone", tag="Someone`s tag", location="Somewhere", avatar="https://static.chipdip.ru/images/layout/noimage/230px.png", stats={followers: 0, views: 0, likes: 0} }){
   return(
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" width="250px"/>
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} width="250px"/>
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
-      <ul className="stats">
+      <ul className={css.stats}>
         <li>
-          <span className="label">Followers: </span>
-          <span className="quantity">{stats.followers}</span>
+          <span className={css.label}>Followers: </span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li>
-          <span className="label">Views: </span>
-          <span className="quantity">{stats.views}</span>
+          <span className={css.label}>Views: </span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
         <li>
-          <span className="label">Likes: </span>
-          <span className="quantity">{stats.likes}</span>
+          <span className={css.label}>Likes: </span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
   )
 };
+
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.node,
+  stats: PropTypes.object
+}
